189273dd343933773e07c0c4f020801506c5e224
Diego Braga <diego.cstbraga@gmail.com>
2020-07-01 23:04:27 -0300

    excluindo arquivos extras

--- index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import Routes from './src/routes';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Routes);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
-import Main from './pages/GenerateNewPasswd';
-
-const App = () => <Main />;
-
-export default App;
