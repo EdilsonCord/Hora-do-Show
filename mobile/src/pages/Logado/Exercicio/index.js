864a5852cb5723ff36d4f0c1721cb008e384b493
Diego Braga <diego.cstbraga@gmail.com>
2020-07-05 19:28:30 -0300

    Tela parcial da aba exercicio

--- mobile/src/pages/Logado/Exercicio/index.js
+++ mobile/src/pages/Logado/Exercicio/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 
 import styles from './styles';
 
-export default function Exercicio({navigation}){
-    return(
-        <View style={styles.container}>
-            {/* <Image 
-            style={{width: 100, height:100}}
-            source={require('../../../assets/faladinho.jpg')} /> */}
-            <Text>Exercício</Text>
-        </View>
-    )
+export default function Exercicio({navigation}) {
+  const dados = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'Treino A',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Treino B',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Treino C',
+    },
+  ]
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.introducao}>
+        <Text style={styles.textoIntroducao}>Bem vindo, Diego</Text>
+        <Text style={styles.textoIntroducao}>
+          Escolha qual treino deseja fazer hoje
+        </Text>
+      </View>
+
+      <View style={styles.faixa}>
+          <Text style={styles.textoFaixa}>Escolha um dos treinos abaixo para alcançar sua meta</Text>
+      </View>
+
+      <FlatList
+        data={dados}
+        style={styles.listarTreinos}
+        renderItem={({item}) => 
+          <TouchableOpacity style={styles.campoTreino}>
+            <Text style={styles.textoCampoTreino}>{item.title}</Text>
+          </TouchableOpacity>
+        }
+      />
+    </SafeAreaView>
+  );
 }
--- mobile/src/pages/Logado/Exercicio/styles.js
+++ mobile/src/pages/Logado/Exercicio/styles.js
@@ -5,10 +5,56 @@ export default StyleSheet.create ({
         flex:1,
         backgroundColor: '#E55F54',
         padding: 20,
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: "center",
         alignItems: "center",
+    },
+    introducao: {
+        width: '100%',
+        // backgroundColor: 'white',
 
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start'
+    },
+    textoIntroducao:{
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    faixa:{
+        marginTop: 25,
+        width: '120%',
+        height: '10%',
+        backgroundColor: '#373737',
 
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textoFaixa:{
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    listarTreinos:{
+        marginTop: 40,
+        width: '100%',
+    },
+    campoTreino:{
+        borderRadius: 8,
+        marginVertical: 10,
+        
+        height: 60,
+        backgroundColor: '#F9EE92',
+
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textoCampoTreino:{
+        color: 'black',
+        fontSize: 30,
+        fontWeight: 'bold',
     }
+    
 })
