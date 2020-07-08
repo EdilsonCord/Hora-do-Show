573e59c134f64ac610156a7843b10390cd95e730
Diego Braga <diego.cstbraga@gmail.com>
2020-07-08 06:16:51 -0300

    Pastas organizadas

---
+++ mobile/src/pages/Deslogado/EnviarCodigo/ConfirmarCodigo/Cadastro/Cadastro.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import {RadioButton} from 'react-native-paper';
+import TextInputMask from 'react-native-text-input-mask';
+import {Picker} from '@react-native-community/picker';
+
+import styles from './styles';
+
+export default function Cadastro({navigation}) {
+  const [value, setValue] = React.useState('Masc');
+
+  const [selectedValue, setSelectedValue] = useState('Meta');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require('../../../../../assets/logooficial.png')}
+          resizeMode="contain"
+          style={styles.logo}
+        />
+
+        <Text style={styles.textoImportante}>CADASTRO</Text>
+      </View>
+
+      <View style={styles.forms}>
+        <TextInput placeholder="Nome" style={styles.insertText} />
+        <TextInput placeholder="Sobrenome" style={styles.insertText} />
+
+        <View style={styles.campoSexo}>
+          <Text style={styles.textoImportante}>Sexo</Text>
+
+          <RadioButton.Group
+            onValueChange={(value) => setValue(value)}
+            value={value}>
+            <View style={styles.radioButton}>
+              <RadioButton value="Masc" />
+
+              <Text style={styles.textoRB}>Masculino</Text>
+
+            </View>
+            <View style={styles.radioButton}>
+              <RadioButton value="Fem" />
+
+              <Text style={styles.textoRB}>Feminino</Text>
+
+            </View>
+          </RadioButton.Group>
+        </View>
+
+        <View style={styles.campoDuplo}>
+          <TextInput
+            placeholder="Altura (cm)"
+            keyboardType={'numeric'}
+            style={styles.insertValuePequeno}
+          />
+          <TextInput
+            placeholder="Peso (kg)"
+            keyboardType={'numeric'}
+            style={styles.insertValuePequeno}
+          />
+        </View>
+
+        <View style={styles.campoDuplo}>
+          <TextInput
+            placeholder="Data de Nasc."
+            style={styles.insertValuePequeno}
+          />
+
+          <Picker
+            placeholder={'meta'}
+            selectedValue={selectedValue}
+            style={styles.insertValuePequeno}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedValue(itemValue)
+            }>
+            <Picker.Item label="JavaScript" value="js" />
+            <Picker.Item label="Java" value="java" />
+          </Picker>
+        </View>
+
+      </View>
+
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.fatButton}
+        onPress={() => navigation.navigate('ContaCriada')}>
+          <Text style={styles.textoFatButton}>FINALIZAR CADASTRO</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.pop()}>
+          <Text style={styles.link}>Voltar</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
