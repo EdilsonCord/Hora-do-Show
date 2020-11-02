eabbec9252cdacb6a435eedeb5ac1bb2ecdd67ac
pepeluiz <pepespim06@gmail.com>
2020-11-02 19:17:49 -0300

    att admin senha para logar comentar

--- backend/admin.js
+++ backend/admin.js
@@ -12,8 +12,28 @@ const adminBro = new AdminBro({
   databases: [mongoose],
   rootPath: '/admin',
   branding: {
-    companyName: 'Pedro Spim e empregados',
+    companyName: 'Hora do Show',
+    logo: 'https://i.imgur.com/ystKdeu.png',
+    softwareBrothers: false
   },
 })
 
+const ADMIN = {
+  email: process.env.ADMIN_EMAIL || 'admin@example.com',
+  password: process.env.ADMIN_PASSWORD || 'lovejs',
+}
+
+const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+  cookieName: process.env.ADMIN_COOKIE_NAME || 'admin-bro',
+  cookiePassword: process.env.ADMIN_COOKIE_PASS || 'supersecret-and-long-password-for-a-cookie-in-the-browser',
+  authenticate: async (email, password) => {
+    if (email === ADMIN.email && password === ADMIN.password) {
+      return ADMIN
+    }
+    return null
+  }
+})
+
 module.exports = adminRouter = AdminBroExpress.buildRouter(adminBro)
+
+//module.exports = router
